630b081fbbeb0f539c5b2526079ecebb1d8b19d5
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-05-02 11:12:28 +0300

    level design

---
+++ Assets/Scripts/SceneRestarter.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneRestarter : MonoBehaviour
+{
+    public void RestartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+}
