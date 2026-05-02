f65fe6e3a622e210b38ebf29606fcfbfa949d9ce
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-05-02 09:56:35 +0300

    .

--- Assets/Scripts/DestroyOnFastCollision.cs
+++ Assets/Scripts/DestroyOnFastCollision.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent( typeof(Collider))]
-public class DestroyOnFastCollision : MonoBehaviour
+public class DestroyOnFastCollision : MonoBehaviour, INumericProperty
 {
     [SerializeField] private float speedThreshold;
     
     [Tooltip("Triggered on collision, passed with relative velocity of the two bodies")]
-    public UnityEvent<Vector3> OnCollision;
+    public UnityEvent<Vector3> OnAnyCollision;
+    public UnityEvent<Vector3> OnFastCollision;
+
+    public float GetNumericPropertyValue() => speedThreshold;
 
     private void OnCollisionEnter(Collision collision)
     {
         var collisionRelativeVelocity = collision.relativeVelocity;
-        OnCollision.Invoke(collisionRelativeVelocity);
+        
+        OnAnyCollision.Invoke(collisionRelativeVelocity);
 
         if (collisionRelativeVelocity.sqrMagnitude > speedThreshold * speedThreshold)
+        {
+            OnFastCollision.Invoke(collisionRelativeVelocity);
             Destroy(gameObject);
+        }
     }
 }
